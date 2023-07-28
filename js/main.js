d38fb76579550aae43b7607491e7dc642b35e62f
sachi Maurya <sachinshivprasad12@gmail.com>
2023-07-28 20:54:59 +0530

    adding slider video

--- js/main.js
+++ js/main.js
@@ -7,3 +7,11 @@ $('.slider').slick({
     autoplay: true,
     arrows: false
 });
+$('.slide-track').slick({
+    dots: false,
+    infinite: true,
+    speed: 300,
+    slidesToShow: 3,
+    adaptiveHeight: true,
+    autoplay: true
+});
